44e84e5d42a9a2bf12525bb3f227ca0a24230169
nivwer <nivwerr@gmail.com>
2023-07-22 10:38:09 -0300

    add imageSearchSlice

---
+++ client/src/features/searchResults/imageSearchSlice.jsx
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const imageSearchSlice = createSlice({
+    name: 'imagesSearch',
+    initialState:[],
+    reducers: {
+        
+    }
+})
+
+export default imageSearchSlice.reducer
