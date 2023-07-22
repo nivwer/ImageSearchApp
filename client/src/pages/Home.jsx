2353b6585c352ceda8962c3cfdc2a625d54a9939
nivwer <nivwerr@gmail.com>
2023-07-22 10:38:39 -0300

    pages Added

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import SearchForm from '../components/Search/SearchForm'
+import { SearchResults } from '../components/Search/SearchResults'
+
+export function Home() {
+  return (
+    <>
+      <div>Home</div>
+      <SearchForm />
+      <SearchResults />
+    </>
+  );
+}
