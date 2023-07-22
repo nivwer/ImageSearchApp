6a86220dab2b88c3ac4d4d60fef8dee15f69a97c
nivwer <nivwerr@gmail.com>
2023-07-22 10:36:17 -0300

    rootReducer.jsx configured and apiSlice added

---
+++ client/src/api/apiSlice.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:8000/images",
+  }),
+  endpoints: (builder) => ({
+    getHello: builder.query({
+      query: () => "/hello",
+    }),
+  }),
+});
+
+export const { useGetHelloQuery } = apiSlice;
---
+++ client/src/app/store/rootReducer.jsx
@@ -0,0 +1,15 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import imageSearchReducer from '../../features/searchResults/imageSearchSlice'
+
+import { apiSlice } from '../../api/apiSlice'
+
+export const store = configureStore({
+     reducer: {
+        api: apiSlice.reducer,
+        images: imageSearchReducer,
+     },
+     middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
+})
+
+setupListeners(store.dispatch)
