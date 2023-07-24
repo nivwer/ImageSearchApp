2d2b1a439b7357ffac8aef208532713604c48fea
nivwer <nivwerr@gmail.com>
2023-07-23 21:16:36 -0300

    Add subroutes to (/home)

--- client/src/routes/Router.jsx
+++ client/src/routes/Router.jsx
@@ -1,20 +1,25 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { Home } from '../pages/Home'
-import { About } from '../pages/About'
-import { Navigation } from '../components/Navigation/Navigation'
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { About } from "../pages/About";
+import { NotFoundPage } from "../pages/NotFoundPage";
+import { Navigation } from "../components/Navigation/Navigation";
+import SearchResults from "../components/Search/SearchResults";
 
 function Router() {
   return (
     <BrowserRouter>
       <Navigation />
       <Routes>
-        <Route path='/' element={<Navigate to='/home' />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/about' element={<About />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home/*" element={<Home />}>
+          <Route path="" element={<SearchResults />} />
+          <Route path="results" element={<SearchResults />} />
+        </Route>
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default Router
+export default Router;
