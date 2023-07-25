29cc856ce64d73c0dea3908a2aa1ef8b56198330
nivwer <nivwerr@gmail.com>
2023-07-24 22:38:20 -0300

    Add ThemeProvider to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import "./styles/index.css";
 import { store } from "./app/store/rootReducer.jsx";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material";
+import theme from './theme.jsx'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
