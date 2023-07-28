0951db57662bfd6cf5e1be61de295b9fc18f7b75
nivwer <nivwerr@gmail.com>
2023-07-28 20:05:04 -0300

    Add query getPopularSearches

--- client/src/api/apiSlice.js
+++ client/src/api/apiSlice.js
@@ -9,7 +9,10 @@ export const apiSlice = createApi({
     getImagesResults: builder.query({
       query: (params) => `/api/v1/search/?query=${params.query}&page=${params.page}`,
     }),
+    getPopularSearches: builder.query({
+      query: (limit) => `/api/v1/searches/popular/?limit=${limit}`,
+    }),
   }),
 });
 
-export const { useGetImagesResultsQuery } = apiSlice;
+export const { useGetImagesResultsQuery, useGetPopularSearchesQuery } = apiSlice;
