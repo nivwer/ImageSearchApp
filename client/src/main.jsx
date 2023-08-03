08fe2a849eebeca487953ae59a908ff9bc271b85
nivwer <nivwerr@gmail.com>
2023-08-03 17:22:44 -0300

    Add CssBaseline

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-import '@fontsource/im-fell-english';
-import '@fontsource/im-fell-english/400-italic.css';
+import "@fontsource/im-fell-english";
+import "@fontsource/im-fell-english/400-italic.css";
 
 import "./styles/index.css";
 import { store } from "./app/store/rootReducer.jsx";
 import { Provider } from "react-redux";
-import { ThemeProvider } from "@mui/material";
-import theme from './theme.jsx'
+import { Container, CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <App />
       </ThemeProvider>
     </Provider>
