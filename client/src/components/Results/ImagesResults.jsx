a5afe6d1cccfb5a85af56f93bc1c44ade58f3529
nivwer <nivwerr@gmail.com>
2023-08-03 17:18:48 -0300

    Responsive ImagesResults update

--- client/src/components/Results/ImagesResults.jsx
+++ client/src/components/Results/ImagesResults.jsx
@@ -1,4 +1,4 @@
-import { Box, ImageList, ImageListItem } from "@mui/material";
+import { Box, ImageList, ImageListItem, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 
 function SearchResults() {
@@ -6,11 +6,27 @@ function SearchResults() {
   const imagesData = useSelector((state) => state.imagesData);
   const results = imagesData.results;
 
+  const isXSmallScreen = useMediaQuery((theme) => theme.breakpoints.only("xs"));
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.only("sm"));
+  const isMediumScreen = useMediaQuery((theme) => theme.breakpoints.up("md"));
+
+  const getCols = () => {
+    if (isMediumScreen) {
+      return 5;
+    } else if (isSmallScreen) {
+      return 4;
+    } else if (isXSmallScreen) {
+      return 2;
+    } else {
+      return 5;
+    }
+  };
+
   if (results) {
     return (
       <>
         <Box sx={{ minHeight: "80vh" }}>
-          <ImageList variant="masonry" cols={5} gap={8}>
+          <ImageList variant="masonry" cols={getCols()} gap={8}>
             {results.map((r) => (
               <ImageListItem key={r.id}>
                 <img
