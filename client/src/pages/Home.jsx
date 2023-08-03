9cb62ce1b7e838c235ddd7f0371f188f40e4a8d1
nivwer <nivwerr@gmail.com>
2023-08-03 17:25:50 -0300

    Updated HomePage

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { useTheme } from "@mui/material/styles";
 import { useState, useEffect } from "react";
 import { Outlet, useSearchParams, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
@@ -16,9 +17,13 @@ import {
   Divider,
   Button,
   Typography,
+  Toolbar,
+  AppBar,
 } from "@mui/material";
 
 export function Home() {
+  //
+  const theme = useTheme();
   // Query for get the Popular Searches.
   const { data: popularSearchesData } = useGetPopularSearchesQuery();
   const dispatch = useDispatch();
@@ -57,7 +62,7 @@ export function Home() {
     // Update parameters state for the query if exist.
     // If not exist, parameters state is default.
     setParams(() => ({
-      query: query ? query : "cats",
+      query: query ? query : "gothic",
       page: page ? page : "1",
     }));
   }, [searchParams]);
@@ -73,51 +78,105 @@ export function Home() {
     dispatch(AddImages(imagesData));
   }, [imagesData]);
 
+  const popularSearches = [
+    "Dog",
+    "Cat",
+    "Car",
+    "Flor",
+    "Gothic",
+    "Black",
+    "Rose",
+    "Magical ",
+  ];
+
+  const NavPopularSearches = () => (
+    <AppBar
+      sx={{ zIndex: 10, display: { xs: "none", md: "block", top: 60, } }}
+      component={"nav"}
+      color="primary"
+    >
+      <Toolbar
+        sx={{
+        }}
+      >
+       
+        <Stack
+          justifyContent={"center"}
+          direction="row"
+          spacing={1}
+          sx={{ width: "100%", mr: "90px" }}
+        >
+          {/* Recommended Popular Searches. */}
+          {popularSearches.map((r, i) => {
+            return (
+              <Button
+                size="small"
+                sx={{ borderRadius: "100px" }}
+                variant="contained"
+                color="secondary"
+                key={i}
+              >
+                <Link
+                  style={{
+                    textDecoration: "none",
+                    color: theme.palette.text.secondary,
+                  }}
+                  to={`/home/results/?query=${r}&page=1`}
+                >
+                  {r}
+                </Link>
+              </Button>
+            );
+          })}
+        </Stack>
+         {/* View all results. */}
+         {/* <Typography variant="subtitle1">RESULTS</Typography> */}
+         
+      </Toolbar>
+    </AppBar>
+  );
+
   // When getting the images data results.
-  if (isLoading) return <div>Loading..</div>;
-  else if (isError) return <div>Error: {error.message}</div>;
+  if (isLoading)
+    return (
+      <>
+        <NavPopularSearches />
+        <div>Loading..</div>
+      </>
+    );
+  else if (isError)
+    return (
+      <>
+        <NavPopularSearches />
+        <div>Error: {error.message}</div>
+      </>
+    );
 
   // Additional logic.
   const totalPages = imagesData.total_pages;
-  const popularSearches = ["Dog", "Cat", "Car", "Flor"];
 
   return (
     <>
+      <NavPopularSearches />
       {/* HomePage Container. */}
-      <Container>
-        <Stack
-          sx={{ m: "0 30px" }}
-          justifyContent={"start"}
-          direction="row"
-          spacing={2}
-        >
-          {/* Recommended Popular Searches. */}
-          <Box sx={{ flexGrow: 1 }}>
-            {popularSearches.map((r, i) => {
-              return (
-                <Button key={i}>
-                  <Link to={`/home/results/?query=${r}&page=1`}>{r}</Link>
-                </Button>
-              );
-            })}
-          </Box>
-          {/* View all results. */}
-          <Typography variant="subtitle1">RESULTS</Typography>
-        </Stack>
+      <Container sx={{ mt: {xs: "75px", md: "150px"}, mb: "75px"}} >
         {/* view Images results. */}
+        <Divider sx={{ m: "40px" }}>{(params.query).toUpperCase()}</Divider>
         <Outlet />
-        <Divider sx={{ m: "40px" }}>CENTER</Divider>
+        <Divider sx={{ m: "40px" }}>{(params.query).toUpperCase()}</Divider>
 
         {/* Page pagination. */}
         <Box sx={{ m: "30px" }}>
           <Stack justifyContent="center" direction="row" spacing={2}>
             <Pagination
+              size="small" 
               page={parseInt(params.page)}
               count={totalPages}
               color="secondary"
               sx={{}}
               renderItem={(item) => (
                 <PaginationItem
+                  sx={{fontSize: {xs: "1.2em", md: "1.4em"}, p: {xs: "0", md: "10px"}}}
                   component={Link}
                   to={`/home/results/?query=${params.query}&page=${item.page}`}
                   {...item}
