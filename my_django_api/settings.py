cbec5058efe31ef600cb85f154c3adc26fd80e80
nivwer <nivwerr@gmail.com>
2023-08-06 23:46:39 -0300

    Update settings, add mongo_middleware

--- my_django_api/settings.py
+++ my_django_api/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'my_django_api.middleware.mongo_middleware.MongoDBMiddleware', # MongoDB connection.
 ]
 
 ROOT_URLCONF = 'my_django_api.urls'
