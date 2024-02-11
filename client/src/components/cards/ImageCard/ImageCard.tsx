708e257d12609555815aed4c032240d3fdd0eeba
nivwer <nivwerr@gmail.com>
2024-02-10 22:58:53 -0300

    updated components.

--- client/src/components/cards/ImageCard/ImageCard.tsx
+++ client/src/components/cards/ImageCard/ImageCard.tsx
@@ -5,7 +5,18 @@ interface IProps {
 }
 
 function ImageCard({ image }: IProps) {
-  return <div key={image.id}>image: {image.id}</div>;
+  return (
+    <div className="flex w-1/2 sm:w-1/3 flex-wrap grayscale hover:grayscale-0">
+      <div className="w-full p-1 md:p-2 h-44  md:h-64">
+        <img
+          src={image.urls.regular}
+          alt=""
+          loading="lazy"
+          className=" block h-full w-full object-cover object-center"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ImageCard;
--- client/src/interfaces/IImage.ts
+++ client/src/interfaces/IImage.ts
@@ -1,3 +1,7 @@
 export interface IImage {
   id: string;
+  urls: {
+    regular: string;
+    small: string;
+  };
 }
