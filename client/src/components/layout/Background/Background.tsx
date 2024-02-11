731dcbb89cddaaabb37d8cd22aaba25969386dd9
nivwer <nivwerr@gmail.com>
2024-02-10 22:54:21 -0300

    Added background.

---
+++ client/src/components/layout/Background/Background.tsx
@@ -0,0 +1,11 @@
+function Background() {
+    return (
+      <div
+        className=" bg-cover bg-bottom h-screen w-screen fixed blur-md -z-50"
+        style={{ backgroundImage: "url('/denny-muller-Qxb2Fh6Aqpg-unsplash.jpg')" }}
+      />
+    );
+  }
+  
+  export default Background;
+  
