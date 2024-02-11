843fcaf559944ecae002f9a6d5bd3fed3bc64157
nivwer <nivwerr@gmail.com>
2024-02-10 22:57:39 -0300

    create SearchForm component.

---
+++ client/src/components/forms/SearchForm/SearchForm.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { BiSearchAlt2 } from "react-icons/bi";
+
+function SearchForm() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [query, setQuery] = useState<string>("");
+
+  const handleOnSubmit = (e: any) => {
+    e.preventDefault();
+    if (query) router.push(`/results?query=${query}&page=1`);
+  };
+
+  useEffect(() => {
+    const queryParam: string | null = searchParams.get("query");
+    if (queryParam) setQuery(queryParam);
+  }, []);
+
+  return (
+    <form onSubmit={handleOnSubmit}>
+      <div className="flex justify-between">
+        <Input
+          type="text"
+          placeholder="Search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="border-r-0 h-12 text-md px-6"
+        />
+        <Button
+          type="submit"
+          variant="outline"
+          size="icon"
+          className="h-12 text-xl border-l-0 w-24"
+          children={<BiSearchAlt2 />}
+        />
+      </div>
+    </form>
+  );
+}
+
+export default SearchForm;
