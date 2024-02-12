e9bc4d1de02c4fb7a8f554e631d3e9b48d8cf89e
nivwer <nivwerr@gmail.com>
2024-02-11 21:59:09 -0300

    create Divider component.

---
+++ client/src/components/Divider/Divider.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+
+interface IProps {
+  children: ReactNode | string;
+}
+
+function Divider({ children }: IProps) {
+  return (
+    <div className="flex w-full py-3 sm:py-6">
+      <div className="grow h-14 flex justify-center items-center px-8  opacity-40">
+        <div className="w-full border h-0"></div>
+      </div>
+      <div className="grow-0 h-14 flex justify-center items-center ">
+        <div>{children}</div>
+      </div>
+      <div className="grow h-14 flex justify-center items-center px-8 opacity-40">
+        <div className="w-full border h-0"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Divider;
