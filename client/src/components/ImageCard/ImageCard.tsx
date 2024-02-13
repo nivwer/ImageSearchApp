8a39e2b2e1eeac2d0363703611187816cd0f4a3b
nivwer <nivwerr@gmail.com>
2024-02-12 22:31:47 -0300

    updated ImageCard component.

--- client/src/components/ImageCard/ImageCard.tsx
+++ client/src/components/ImageCard/ImageCard.tsx
@@ -9,27 +9,46 @@ interface IProps {
 }
 
 function ImageCard({ image }: IProps) {
+  const [isHovered, setIsHovered] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 500); // Ajusta el tiempo según sea necesario
+    }, 500);
 
     return () => clearTimeout(timer);
-  }, []); 
+  }, []);
+
+  const handleMouseLeave = () => {
+    setIsHovered(true);
+
+    setTimeout(() => {
+      setIsHovered(false);
+    }, 1500); 
+  };
 
   return (
-    <div className="flex w-1/2 sm:w-1/3 flex-wrap grayscale hover:grayscale-0">
+    <div
+      className="flex w-1/2 sm:w-1/3 flex-wrap grayscale "
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={handleMouseLeave}
+      style={{
+        filter: isHovered ? "grayscale(0%)" : "grayscale(100%)",
+        transition: "filter 1s ease", // Ajusta la duración según sea necesario
+      }}
+    >
       <div className="w-full p-1 md:p-2 h-44  md:h-64">
         {isLoading && <Skeleton className=" h-full w-full bg-white/30" />}
-        <img
-          src={image.urls.regular}
-          alt=""
-          loading="lazy"
-          className=" block h-full w-full object-cover object-center"
-          style={{ opacity: isLoading ? 0 : 1 }}
-        />
+        <a href={image.links.html} target="_blank">
+          <img
+            src={image.urls.regular}
+            alt=""
+            loading="lazy"
+            className=" block h-full w-full object-cover object-center"
+            style={{ opacity: isLoading ? 0 : 1 }}
+          />
+        </a>
       </div>
     </div>
   );
